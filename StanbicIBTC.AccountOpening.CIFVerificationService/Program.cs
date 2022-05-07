7237d91fd7c4cc6322a68d3d9bf128fbd79420da
Michael Boniface <Michael.Boniface@stanbicibtc.com>
2022-05-07 01:37:14 -0700

    Added Cif Verification

---
+++ StanbicIBTC.AccountOpening.CIFVerificationService/Program.cs
@@ -0,0 +1,10 @@
+using StanbicIBTC.AccountOpening.CIFVerificationService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
