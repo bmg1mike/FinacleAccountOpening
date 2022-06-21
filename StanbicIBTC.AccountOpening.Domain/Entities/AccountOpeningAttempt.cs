8fea234595911b3563872c689eb158fbafd73cfe
Michael Boniface <Michael.Boniface@stanbicibtc.com>
2022-06-21 01:43:04 -0700

    Initial commit

--- StanbicIBTC.AccountOpening.Domain/Entities/AccountOpeningAttempt.cs
+++ StanbicIBTC.AccountOpening.Domain/Entities/AccountOpeningAttempt.cs
@@ -6,8 +6,6 @@ public partial class AccountOpeningAttempt
    [BsonRepresentation(BsonType.ObjectId)]
 
    public string AccountOpeningAttemptId { get; set;}
-   //public string FirstName { get; set;}
-   //public string LastName { get; set;}
    public string Bvn { get; set;}
    public string Response { get; set;}
    public bool IsSuccessful { get; set;} = false;
